8c40d4bf31038dbbbd14f9412886c02f62b0e13e
hvelarderiquelme <hvelardeab@gmail.com>
2025-05-30 12:46:15 -0600

    corrections to welcome page

--- chamber/scripts/person-info.js
+++ chamber/scripts/person-info.js
@@ -1,13 +1,15 @@
 // const getString = window.location.search;
 // console.log(getString);
 
-const registrationDate = new Date();
+
 const personInfo = new URLSearchParams(window.location.search);
 const personalInfo = document.querySelector(".information");
+const registrationDate = new Date();
+
 
 personalInfo.innerHTML = `<p>Name: ${personInfo.get("first")}</p>
 <p>Lastname: ${personInfo.get("last")}</p>
 <p>E-mail: ${personInfo.get("email")}</p>
-<p>Phone Number: ${personInfo.get("phone")}
-<p>Company: ${personInfo.get("company")}
-<p>Registered on: ${registrationDate}`;
+<p>Phone Number: ${personInfo.get("phone")}</p>
+<p>Company: ${personInfo.get("company")}</p>
+<p>Registered on: ${registrationDate}</p>`;
---
+++ chamber/scripts/registration.js
@@ -0,0 +1,6 @@
+
+const registrationDate = new Date();
+const time = document.getElementsByName("time-stamp");
+console.log(registrationDate);
+time.value = `${registrationDate}`;
+
