f91f2ee25d8cbec13fd3eda1930d352dc44bdeed
hvelarderiquelme <hvelardeab@gmail.com>
2025-06-13 14:14:45 -0600

    final with corrections

---
+++ video_demos/scripts/events.js
@@ -0,0 +1,53 @@
+import {events} from "../data/events.mjs"
+// console.log(events);
+const closeButton = document.querySelector("#modal button");
+const modal = document.querySelector("#modal");
+const hiddenName = document.querySelector("#hiddenName");
+const hiddenDate = document.querySelector("#hiddenDate");
+const hiddenTime = document.querySelector("#hiddenTime");
+
+closeButton.addEventListener("click", () => {modal.close()});
+
+const displayEvents = (events)=>{
+    //call for each events in the event array
+    events.forEach(event => {
+        let section = document.createElement("section");
+        let name = document.createElement("h3");
+        let when = document.createElement("p");
+        let what = document.createElement("p");
+        let outcome = document.createElement("p");
+        let input = document.createElement("input");
+        
+
+        name.innerHTML = `${event.event}`;
+        when.innerHTML = `Date: ${event.date} <br> Time: ${event.time}`;
+        what.innerHTML = `Activity: ${event.description}`;
+        outcome.innerHTML = `Outcome: ${event.goal}`;
+        input.setAttribute("type","submit");
+        input.setAttribute("value","Register");
+        input.setAttribute("id","registration");
+        input.addEventListener("click", () => {showModalMessage(event)});
+        
+        section.appendChild(name);
+        section.appendChild(when);
+        section.appendChild(what);
+        section.appendChild(outcome);
+        section.appendChild(input);
+        
+        document.querySelector("#events").appendChild(section);
+        
+        
+    });
+
+}
+displayEvents(events);
+
+function showModalMessage(event){
+    hiddenName.setAttribute("value", `${event.event}`);
+    hiddenDate.setAttribute("value", `${event.date}`);
+    hiddenTime.setAttribute("value",`${event.time}`);
+    modal.showModal();
+    
+}
+
+
---
+++ video_demos/scripts/lastmodified.js
@@ -0,0 +1,16 @@
+//Footer copyright and last modified information
+const ElmtYear = document.querySelector("#current-year");
+const ElmtLastModified = document.querySelector("#last-modified");
+const currentYear = new Date();
+const lastModified = document.querySelector("#last-modified");
+const date = new Date(document.lastModified);
+let year = currentYear.getFullYear();
+const spanElmt = document.createElement("span");
+
+document.querySelector('#current-year').innerHTML = '&COPY;' +  year + ' Medicine Hat Writers Club';
+document.querySelector('#last-modified').innerHTML = date;
+ElmtYear.appendChild(spanElmt);
+ElmtLastModified.appendChild(spanElmt);   
+
+
+
