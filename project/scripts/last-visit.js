be28cc7c781777a07eb2a823dcadb71da94f450e
hvelarderiquelme <hvelardeab@gmail.com>
2025-06-14 10:35:10 -0600

    changesin menu

--- project/scripts/last-visit.js
+++ project/scripts/last-visit.js
@@ -48,7 +48,7 @@ function modalVisit(days, numVisits){
         h1.innerHTML = `Hi again! Have you joined yet?`;
         p.innerHTML = `We hope you enjoy your visit. `;
     }else if(parseInt(days) == 1){
-        h1.innerHTML = `Well, Hello There! Great to see you again!`;
+        h1.innerHTML = `Hello There! Great to see you again!`;
         p.innerHTML = `You last visited ${parseInt(days)} day ago.`;
         }
         else
